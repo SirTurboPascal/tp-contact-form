714fc7cce650fee7c4fed597312e4c56da29fb18
Theo Gernke <theogernke@icloud.com>
2025-07-16 09:47:49 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next';
+import { FC, ReactNode } from 'react';
+
+import '@/styles/globals.css';
+
+const metadata: Metadata = {
+	title: 'Contact Form',
+};
+
+interface ILayoutProps {
+	children: ReactNode;
+}
+
+const Layout: FC<ILayoutProps> = ({ children }) => {
+	return (
+		<html className='overflow-hidden bg-green-200'>
+			<body className='font-karla h-screen antialiased'>{children}</body>
+		</html>
+	);
+};
+
+export default Layout;
+export { metadata };

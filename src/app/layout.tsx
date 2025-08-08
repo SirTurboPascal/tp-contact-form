e33c576ae9e83f3e288142224817f6f19a270656
Theo Gernke <theogernke@icloud.com>
2025-08-08 21:02:33 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+import '@/styles/globals.css';
+
+export const metadata: Metadata = {
+	title: 'Contact Form',
+};
+
+type Props = {
+	children: ReactNode;
+};
+
+export default function ({ children }: Props) {
+	return (
+		<html className='bg-green-200'>
+			<body className='h-dvh'>{children}</body>
+		</html>
+	);
+}

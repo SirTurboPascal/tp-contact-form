452ac189d9a94de864555fe75285f5598f1ae503
Theo Gernke <theogernke@icloud.com>
2026-04-28 12:32:46 +0200

    Fixes missing "type" prop on Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,9 @@ type ButtonProps = {
 	text: string;
 } & Pick<ComponentPropsWithoutRef<'button'>, 'onClick' | 'type'>;
 
-export default function ({ onClick, text }: ButtonProps) {
+export default function ({ onClick, text, type }: ButtonProps) {
 	return (
-		<button className='group relative flex h-[59px] cursor-pointer items-center overflow-hidden rounded-lg bg-green-600 select-none' onClick={onClick}>
+		<button className='group relative flex h-[59px] cursor-pointer items-center overflow-hidden rounded-lg bg-green-600 select-none' onClick={onClick} type={type}>
 			<div className='absolute inset-0 bg-black opacity-0 transition-opacity duration-100 outline-none group-hover:opacity-50' />
 
 			<span className='font-karla relative grow text-center text-[18px]/[150%] font-bold text-white antialiased'>{text}</span>
